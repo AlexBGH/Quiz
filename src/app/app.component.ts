6285bdf516ecac1b06f5f69601a9b98bd91c9c7e
Ryukishuu <ryukishu.besse@gmail.com>
2024-04-14 18:46:21 +0200

    ajout des objets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, CommonModule, FormsModule],
+  imports: [RouterOutlet, NgFor, NgIf, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,23 +14,88 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   quizName!: string;
   question!: string;
-  playerResponse!: string;
-  response!: string;
-  quizs!: {question: string, response: string}[];
+  playerAnswer!: string;
+  answer!: string;
+  visible!: boolean;
+  hasAnswered!: boolean;
+  hasGoodAnswer!: boolean;
+  useFourChoices!: boolean;
+  fourChoices!: [];
+  quizs!: {
+    question: string, 
+    playerAnswer: string, 
+    answer: string, visible: boolean, 
+    hasAnswered: boolean, 
+    hasGoodAnswer: boolean, 
+    useFourChoices: boolean,
+    fourChoises: string[]
+  }[];
+  points: number = 0;
 
   ngOnInit() {
     this.quizName = "ANIME";
     this.quizs = [
-      {question: "Quel est le résultat de 1+1 ?", response: "2"},
-      {question: "Quel est la distance du couloir de Misha ?", response: "300 mètres"},
-      {question: "Qui a dit : <<j'ai jamais dit ça>> ?", response: "Ninu"},
+      {
+        question: "Quel est le résultat de 1+1 ?", 
+        playerAnswer: "", answer: "2", 
+        visible: true, 
+        hasAnswered: false, 
+        hasGoodAnswer: false, 
+        useFourChoices: false, 
+        fourChoises: [
+          "4", 
+          "2", 
+          "6", 
+          "8"
+        ]},
+      {
+        question: "Quel est le résultat de 2+2 ?", 
+        playerAnswer: "", 
+        answer: "4", 
+        visible: true, 
+        hasAnswered: false, 
+        hasGoodAnswer: false, 
+        useFourChoices: false, 
+        fourChoises: [
+          "6", 
+          "4", 
+          "2", 
+          "8"
+        ]},
+      {
+        question: "Quel est le résultat de 3+3 ?", 
+        playerAnswer: "", 
+        answer: "6", 
+        visible: true, 
+        hasAnswered: false, 
+        hasGoodAnswer: false, 
+        useFourChoices: false, 
+        fourChoises: [
+        "8", 
+        "4", 
+        "6", 
+        "2"
+      ]},
     ]
   }
-  onClick() {
-    this.onSubmit();
+  onClickPlayerAnswer(playerAnswer: string, i: number) {
+    if(!this.quizs[i].hasAnswered) {
+      this.onSubmit(playerAnswer, i);
+      this.quizs[i].hasAnswered = !this.quizs[i].hasAnswered;
+    }
   }
-  onSubmit() {
-    alert(this.playerResponse);
+  onClickFourChoices(useFourChoices: boolean, i: number) {
+    this.quizs[i].useFourChoices = !useFourChoices;
+  }
+  onSubmit(playerAnswer: string, i: number) {
+    this.quizs[i].playerAnswer = playerAnswer;
+    this.quizs[i].hasGoodAnswer = (playerAnswer === this.quizs[i].answer)
+    if(this.quizs[i].hasGoodAnswer && !this.quizs[i].useFourChoices) {
+      this.points = this.points + 3;
+    } else if (this.quizs[i].hasGoodAnswer && this.quizs[i].useFourChoices) {
+      this.points = this.points + 1;
+    }
+    this.quizs[i].visible = !this.quizs[i].visible;
   }
   title = 'Quiz';
 }
