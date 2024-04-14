14b47177007b35680f36f87404291e9d439fdada
Ryukishuu <ryukishu.besse@gmail.com>
2024-04-14 14:05:47 +0200

    style v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  quizName!: string;
+  question!: string;
+  response!: string;
+  quizs!: {question: string, response: string}[]
+
+  ngOnInit() {
+    this.quizName = "BLAGUE";
+    this.quizs = [
+      {question: "Quel est le résultat de 1+1 ?", response: "2"},
+      {question: "Quel est la distance du couloir de Misha ?", response: "300 mètres"},
+      {question: "Qui a dit : <<j'ai jamais dit ça>> ?", response: "Ninu"},
+    ]
+  }
+  
   title = 'Quiz';
 }
